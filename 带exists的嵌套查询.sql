e0b572f44f691ab0916d586d9a29b184a2689de3
LucasZhengrui <lucaszhengrui@gmail.com>
2019-10-17 10:49:32 +0800

    错误分析与收集

--- 带exists的嵌套查询.sql
+++ 带exists的嵌套查询.sql
@@ -28,3 +28,40 @@
 -- 200215121
 -- 200215122
 -- 200215123
+
+--错误查询2
+select stu_id
+from elective_course
+where stu_id in (select B.stu_id
+				from elective_course A,elective_course B
+				where A.stu_id='200215122'and A.course_id=B.course_id
+				)
+group by stu_id;
+
+-- stu_id
+-- 200215121
+-- 200215122
+-- 200215123
+
+--错误查询3
+select distinct stu_id
+from elective_course
+where course_id in(select course_id
+from elective_course
+where stu_id='200215122')and course_id not in('200215122')
+
+-- stu_id
+-- 200215121
+-- 200215122
+-- 200215123
+
+--错误查询4
+select distinct stu_id
+from elective_course x
+where course_id in(select course_id
+				  from elective_course y
+				  where x.course_id=y.course_id and x.stu_id!='200215122');
+--group by stu_id;
+-- stu_id
+-- 200215121
+-- 200215123
